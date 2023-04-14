3104db4021c36810014b7755e224b895cd1559a5
cook <send2ugfd@gmail.com>
2023-04-15 03:48:04 +0900

    스프링 MVC 웹 페이지 만들

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,23 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+
+@Data // 위험할 수 있음(별로 사용하지 않는걸 추천)
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+
+    public Item() {
+
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
